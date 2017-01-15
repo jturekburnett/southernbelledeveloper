b5420f74010ba95655404962dcbe79e1e94a4aea
Jessica Burnett <jturekburnett@hotmail.com>
2017-01-14 22:19:20 -0700

    Remove commented out image code.

--- theme/header.php
+++ theme/header.php
@@ -1,7 +1,6 @@
 <!-- Header -->
 <header>
     <h1>Jessica Burnett</h1>
-    <!--<img class="magnolia" src="/SouthernBelleDeveloper/images/magnoliaColoredSmall.png" title="magnolia">-->
     <h2>Southern Belle Developer</h2>
 </header>
 <!-- End Header -->
