2f0d8121f19ede8e0aaf32fb2f46e53ce2a77b4e
Jessica Burnett <jturekburnett@hotmail.com>
2017-01-14 22:18:22 -0700

    Add image; with commented out code for alternate image-adding method.

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="/SouthernBelleDeveloper/style.css" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Give+You+Glory" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dawning+of+a+New+Day|Give+You+Glory|Raleway" rel="stylesheet">
 </head>
 <body>
     <?php
@@ -12,5 +11,10 @@
     include '/theme/navbar.php';
     include '/theme/footer.php';
 ?>
+    <main>
+        <?php
+        include '/Home/objective.php';
+        ?>
+    </main>
     </body>
 </html>
--- theme/header.php
+++ theme/header.php
@@ -1,6 +1,7 @@
 <!-- Header -->
 <header>
     <h1>Jessica Burnett</h1>
+    <!--<img class="magnolia" src="/SouthernBelleDeveloper/images/magnoliaColoredSmall.png" title="magnolia">-->
     <h2>Southern Belle Developer</h2>
 </header>
 <!-- End Header -->
