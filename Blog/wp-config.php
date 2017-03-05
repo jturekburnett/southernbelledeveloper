45292b078f21ac357d2e56cf35d923c11c5d1050
Jessica Burnett <jturekburnett@hotmail.com>
2017-03-04 21:24:36 -0700

    config the database for server

--- Blog/wp-config.php
+++ Blog/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'SouthernBelleWP');
+define('DB_NAME', 'db672286212');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'dbo672286212');
 
 /** MySQL database password */
 define('DB_PASSWORD', '3Nephi1248');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db672286212.db.1and1.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
