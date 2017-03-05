390678e96ffd8da3b9911e4543cb09a6e95d6761
Jessica Burnett <jturekburnett@hotmail.com>
2017-03-04 19:49:47 -0700

    Single blog post working

--- Blog/blog.php
+++ Blog/blog.php
@@ -1,4 +1,17 @@
-<p> what the heck </p>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php if (is_single()): ?>
+            <h1> <?php the_title(); ?> </h1>
+            <?php the_content(); ?>
+        <?php else: ?>
+            <div class="date"><?php the_date(); ?></div>
+            <h1> <?php the_title(); ?> </h1>
+            <p> <?php the_excerpt(); ?> </p>
+            <?php the_tags(); ?>
+        <?php endif; ?>
+    <?php endwhile;
+else: ?>
+    <p> Sorry, there are no posts at this time.</p>
+<?php endif; ?>
 
 
 
