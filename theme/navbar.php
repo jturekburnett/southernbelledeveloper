eb70b3fe8693c575871794f8e1afff7af039e4b7
Jessica Burnett <jturekburnett@hotmail.com>
2017-09-21 22:46:55 -0500

    Merge origin/master
    Open Programming link in new tab.
    
    Conflicts:
    	.gitignore

--- theme/navbar.php
+++ theme/navbar.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href='/About/'>Resume</a></li>
             <li><b>&middot;</b></li>
-            <li><a href='https://github.com/jturekburnett'>Programming</a></li>
+            <li><a href='https://github.com/jturekburnett' target="_blank">Programming</a></li>
             <li><b>&middot;</b></li>
             <li><a href='/Blog/index.php'>Blog</a></li>
         </ul>
