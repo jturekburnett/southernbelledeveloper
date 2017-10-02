47b9eaf1f8bb1a35e1286dc9da6bbb351b0b9145
Jessica Burnett <jturekburnett@hotmail.com>
2017-10-02 17:28:29 -0500

    attempt to fix stylesheet
    Conflicts:
    	.gitignore

--- Blog/wp-content/themes/SouthernBelle/functions.php
+++ Blog/wp-content/themes/SouthernBelle/functions.php
@@ -1,7 +1,11 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'twentysixteen', '../../../../../style.css' );
-
+    $parent_style = 'twentysixteen';
+    wp_enqueue_style( $parent_style, '../../../../style.css' );
+    wp_enqueue_style( 'southern-belle-style',
+        '../../../../style.css',
+        array( $parent_style )
+    );
 }
 ?>
